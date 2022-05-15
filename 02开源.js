e1b0860bdeddd3e7d6ef039545418255bd700bfb
xhzwan <1282978595@qq.com>
2022-05-15 20:37:09 +0800

    nihh

---
+++ 02开源.js
@@ -0,0 +1,34 @@
+// 开源是指不仅提供程序还提供程序的源代码 代码公开 任何人都可以查看修改使用
+// 闭源是只提供程序，不提供源代码  只有作者能对源代码进行修改 只有作者能看到闭源软件的代码
+
+// 开源并不意味着完全没有限制，为了限制使用者的使用范围和保护作者的权利，
+// 每个开源项目都应该遵守开源许可协议（ Open Source License ）。
+
+// 常见的 5 种开源许可协议
+// BSD（Berkeley Software Distribution）
+//  Apache Licence 2.0
+// GPL（GNU General Public License）  具有传染性的一种开源协议，不允许修改后和衍生的代码做为闭源的商业软件发布和销售使用 GPL 的最著名的软件项目是：Linux
+// LGPL（GNU Lesser General Public License）
+// MIT（Massachusetts Institute of Technology, MIT） 是目前限制最少的协议，唯一的条件：在修改后的代码或者发行包中，必须包含原作者的许可信息使用 MIT 的软件项目有：jquery、Node.js
+
+// 核心思想是“我为人人，人人为我”
+// 开源给使用者更多的控制权
+// 开源让学习变得容易
+// 开源才有真正的安全
+
+// 开源项目托管平台
+// Github（全球最牛的开源项目托管平台，没有之一）
+// Gitlab（对代码私有性支持较好，因此企业用户较多）
+// Gitee（又叫做码云，是国产的开源项目托管平台。访问速度快、纯中文界面、使用友好）
+
+// Github 上的远程仓库，有两种访问方式，分别是 HTTPS 和 SSH。它们的区别是：
+// HTTPS：零配置；但是每次访问仓库时，需要重复输入 Github 的账号和密码才能访问成功
+
+
+//SSH：需要进行额外的配置；但是配置成功后，每次访问仓库时，不需重复输入 Github 的账号和密码 （推荐）
+
+// git remote -v   查看关联网址
+// git remote（远程） add（增加） origin（别名，可改） https://gitee.com/xhzwan/wan.git （路径）  关联本地和远程仓库的
+// git  branch -M main   默认的master分支名称改为main
+// git push(推送) -u（第一次添加必须加） origin（代指路径地址） main（代指一个分支）
+// git push 同步云端 
